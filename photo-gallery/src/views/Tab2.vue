5eaae6b0758d1f5b58b3125163b66d1d4c9b0741
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-10-26 13:12:38 +0200

    Taking photos and photo gallery

--- photo-gallery/src/views/Tab2.vue
+++ photo-gallery/src/views/Tab2.vue
@@ -11,29 +11,41 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-    </ion-content>
-    <ion-content :fullscreen="true">
-  <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+  <ion-grid>
+    <ion-row>
+      <ion-col size="6" :key="photo" v-for="photo in photos">
+        <ion-img :src="photo.webviewPath"></ion-img>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+ <ion-fab vertical="bottom" horizontal="center" slot="fixed">
     <ion-fab-button @click="takePhoto()">
       <ion-icon :icon="camera"></ion-icon>
     </ion-fab-button>
   </ion-fab>
 </ion-content>
+    
+ 
   </ion-page>
 </template>
 
 <script lang="ts">
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
 import { camera, trash, close } from 'ionicons/icons';
 import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon,
          IonToolbar, IonTitle, IonContent, IonGrid, IonRow,
-         IonCol, IonImg } from '@ionic/vue';
+         IonCol, IonImg} from '@ionic/vue';
 
 export default  {
   name: 'Tab2',
   components: { IonPage, IonHeader, IonFab, IonFabButton, IonIcon,
          IonToolbar, IonTitle, IonContent},
   setup() {
+    const { photos, takePhoto } = usePhotoGallery();
+
     return {
+      photos,
+      takePhoto,
       camera, trash, close
     }
   }
